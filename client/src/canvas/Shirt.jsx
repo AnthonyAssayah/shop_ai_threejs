f7cd5a874b0a035f88316c26527a74c56cf8f6db
Anthony <anthonyassayah14@gmail.com>
2024-05-07 16:17:57 +0300

    shirt features and visualization

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,13 +1,32 @@
 // shirt model
 
-import React from 'react'
+import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
+import state from "../store";
 
 function Shirt() {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+
+  const LogoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+    </group>
+  );
 }
 
-export default Shirt
+export default Shirt;
